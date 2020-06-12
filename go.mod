ed902803b3e1decef1fc96e7595c9a77105fe744
Shahin Safaroff <shahinsafaroff@gmail.com>
2020-06-12 22:49:18 +0400

    Final Bonus system

--- go.mod
+++ go.mod
@@ -1,1 +1,3 @@
 module github.com/shahinsafaroff/go_lesson_1_task_1
+
+go 1.14
--- main.go
+++ main.go
@@ -1,5 +1,16 @@
 package main
 
 func main() {
-	
+	purchase := 33_33
+	percent := 1
+	limit := 100
+	bonus := (purchase * percent)/limit
+
+	if bonus < limit {
+		println(bonus)
+	} else if bonus > limit {
+		bonus := limit
+		println(bonus)
+	}
 }
+
