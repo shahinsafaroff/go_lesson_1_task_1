c239631e7a45c1adc642c2e073787486cb3315c1
Shahin Safaroff <shahinsafaroff@gmail.com>
2020-06-12 23:46:39 +0400

    Formatted

--- main.go
+++ main.go
@@ -2,9 +2,9 @@ package main
 
 func main() {
 	var (
-		purchase float64   = 33_33.0
-		percent  float64   = 1.0
-		limit    float64   = 100.0
+		purchase float64 = 33_33.0
+		percent  float64 = 1.0
+		limit    float64 = 100.0
 		bonus    float64 = (purchase * percent) / limit
 	)
 
