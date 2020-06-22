0a519f3cc6b9f2a449a694362f4531a29a7674b9
Shahin Safaroff <shahinsafaroff@gmail.com>
2020-06-22 12:55:42 +0400

    Bonus can't be a ZERO

--- main.go
+++ main.go
@@ -1,15 +1,17 @@
 package main
 
 func main() {
-	purchase := 33_33
+	purchase := 3333
 	percent := 1
 	limit := 100
 	bonus := (purchase * percent) / limit
 
-	if bonus < limit {
+	if bonus < limit || bonus >= 0 {
 		println(bonus)
 	} else if bonus > limit {
 		bonus := limit
 		println(bonus)
+	} else {
+		println ("No Bonus this time")
 	}
 }
